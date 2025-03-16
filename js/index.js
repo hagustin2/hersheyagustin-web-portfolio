eed8c83944c8d2273f3e486a5584ced08fa6b72d
hagustin2 <hagustin2@nait.ca>
2025-03-16 16:52:44 -0600

    c

--- js/index.js
+++ js/index.js
@@ -14,3 +14,19 @@ window.addEventListener('load', () => {
   typeMessage1();
 
 });
+
+const text2 = "Let’s collaborate and create something amazing!";
+let index2 = 0;
+const typingElement2 = document.querySelector('.typing-text2');
+
+function typeMessage2() {
+  if (index2 < text2.length) {
+    typingElement2.textContent += text2.charAt(index2);
+    index2++;
+    setTimeout(typeMessage2, 50); 
+  }
+}
+window.addEventListener('load', () => {
+  typeMessage1(); 
+  setTimeout(typeMessage2, text1.length * 50); 
+});
