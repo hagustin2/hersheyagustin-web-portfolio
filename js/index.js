aefad7803afda3575cc41e632b62c54f2b8bbb18
hagustin2 <hagustin2@nait.ca>
2025-03-16 16:54:42 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,17 @@
 // For the first typing effect (for the paragraph)
-const text = "I design and develop optimized and user-friendly websites.";
-let index = 0;
-const typingElement = document.querySelector('.typing-text'); 
+const text1 = "I design and develop optimized and user-friendly websites.";
+let index1 = 0;
+const typingElement1 = document.querySelector('.typing-text'); 
 
 function typeMessage1() {
-  if (index < text.length) {
-    typingElement.textContent += text.charAt(index);
-    index++;
+  if (index1 < text1.length) {
+    typingElement1.textContent += text1.charAt(index1);
+    index1++;
     setTimeout(typeMessage1, 50);
   }
 }
-window.addEventListener('load', () => {
-  typeMessage1();
 
-});
-
+// For the second typing effect (for the paragraph 2)
 const text2 = "Let’s collaborate and create something amazing!";
 let index2 = 0;
 const typingElement2 = document.querySelector('.typing-text2');
@@ -26,7 +23,9 @@ function typeMessage2() {
     setTimeout(typeMessage2, 50); 
   }
 }
+
+// Wait until the page is fully loaded before starting both typing effects
 window.addEventListener('load', () => {
-  typeMessage1(); 
-  setTimeout(typeMessage2, text1.length * 50); 
+  typeMessage1(); // Start typing effect for the first paragraph
+  setTimeout(typeMessage2, text1.length * 50); // Start typing effect for the second paragraph after the first one finishes
 });
